d093969c562726eff254760baead0c81f7d170a3
akma <valikhanovnaoa@gmail.com>
2023-10-06 00:42:00 +0600

    myproject

--- cmd/api/cameras.go
+++ cmd/api/cameras.go
@@ -7,45 +7,17 @@ import (
 	"strconv"
 )
 
-// Create a new vintage camera item and return a placeholder response.
 func (app *application) createCameraHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Create a new vintage camera item")
+	fmt.Fprintln(w, "create a new camera")
 }
 
-// Show the details of a vintage camera item by ID.
-func (app *application) getCameraHandler(w http.ResponseWriter, r *http.Request) {
+func (app *application) showCameraHandler(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	id, err := strconv.Atoi(params["id"])
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
 		return
 	}
-	fmt.Fprintf(w, "Show the details of vintage camera item with ID %d\n", id)
-}
-
-// List all vintage camera items.
-func (app *application) listCamerasHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "List all vintage camera items")
-}
-
-// Update the details of a vintage camera item by ID.
-func (app *application) updateCameraHandler(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	id, err := strconv.Atoi(params["id"])
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
-		return
-	}
-	fmt.Fprintf(w, "Update vintage camera item with ID %d\n", id)
-}
-
-// Delete a vintage camera item by ID.
-func (app *application) deleteCameraHandler(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	id, err := strconv.Atoi(params["id"])
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
-		return
-	}
-	fmt.Fprintf(w, "Delete vintage camera item with ID %d\n", id)
+	// Otherwise, interpolate the movie ID in a placeholder response.
+	fmt.Fprintf(w, "show the details of movie %d\n", id)
 }
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -8,12 +8,8 @@ import (
 func (app *application) routes() http.Handler {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/v1/healthcheck", app.healthcheckHandler).Methods("GET")
-	router.HandleFunc("/v1/cameras", app.listCamerasHandler).Methods("GET")
-	router.HandleFunc("/v1/cameras", app.createCameraHandler).Methods("POST")
-	router.HandleFunc("/v1/cameras/{id:[0-9]+}", app.getCameraHandler).Methods("GET")
-	router.HandleFunc("/v1/cameras/{id:[0-9]+}", app.updateCameraHandler).Methods("PUT")
-	router.HandleFunc("/v1/cameras/{id:[0-9]+}", app.deleteCameraHandler).Methods("DELETE")
-
+	router.HandleFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	router.HandleFunc(http.MethodPost, "/v1/cameras", app.createCameraHandler)
+	router.HandleFunc(http.MethodGet, "/v1/cameras/:id", app.showCameraHandler)
 	return router
 }
