6be5af089e55be977d7b79d16fac7d3c18167202
akma <valikhanovnaoa@gmail.com>
2023-11-04 23:36:36 +0600

    myproject
    Ch 7-12 full

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,12 +1,33 @@
 package main
 
 import (
-	"github.com/julienschmidt/httprouter"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
 func (app *application) routes() http.Handler {
-	router := httprouter.New()
+	router := mux.NewRouter()
+
+	router.NotFoundHandler = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowedHandler = http.HandlerFunc(app.methodNotAllowedResponse)
+
+	router.Handle("/v1/healthcheck", http.HandlerFunc(app.healthcheckHandler)).Methods(http.MethodGet)
+	router.HandleFunc("/v1/movies", app.listCameraHandler).Methods(http.MethodGet)
+	router.Handle("/v1/cameras", http.HandlerFunc(app.createCameraHandler)).Methods(http.MethodPost)
+	router.Handle("/v1/cameras/{id:[0-9]+}", http.HandlerFunc(app.showCameraHandler)).Methods(http.MethodGet)
+	router.HandleFunc("/v1/movies/{id:[0-9]+}", app.updateCameraHandler).Methods(http.MethodPut)
+	router.HandleFunc("/v1/movies/{id:[0-9]+}", app.deleteCameraHandler).Methods(http.MethodDelete)
+
+	http.ListenAndServe(":4000", router)
+
+	return app.recoverPanic(app.rateLimit(router))
+}
+
+/*
+
+func (app *application) routes() http.Handler {
+	router := mux.NewRouter()
+
 
 	router.NotFound = http.HandlerFunc(app.notFoundResponse)
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
@@ -20,3 +41,4 @@ func (app *application) routes() http.Handler {
 
 	return app.recoverPanic(app.rateLimit(router))
 }
+*/
